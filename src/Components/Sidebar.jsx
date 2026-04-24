14413a0d249f4e77ef2a690cf47a2f279882bf5f
Riccardo Cardia <cardiariccardo94@gmail.com>
2026-04-24 18:33:50 +0200

    Sidebar sticky

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router"
-export default function Sidebar({genres}) {
+import { Link } from "react-router";
+export default function Sidebar({ genres }) {
   return (
     <>
-    <nav className="h-screen bg-gray-400 ">
-      <ul className="px-5">
-        {
-          genres.map((genre)=> {
+      <nav className="h-screen bg-gray-400 py-5 ">
+        <ul className="px-5">
+          {genres.map((genre) => {
             return (
-              <li key={genre.id} className="mb-2"><Link to={`genre/${genre.slug}`}>{genre.name}</Link></li>
-            )
-          })
-        }
-      </ul>
-    </nav>
+              <li key={genre.id} className="mb-2">
+                <Link to={`genre/${genre.slug}`}>{genre.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </>
-  )
+  );
 }
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -3,12 +3,12 @@ import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Sidebar from "../Components/Sidebar";
 export default function Layout() {
-  const genres = useLoaderData()
+  const genres = useLoaderData();
   return (
     <>
       <Navbar />
       <section className="grid grid-cols-7 gap-4">
-        <div>
+        <div className="sticky top-20 self-start">
           <Sidebar genres={genres} />
         </div>
         <div className="col-span-6">
